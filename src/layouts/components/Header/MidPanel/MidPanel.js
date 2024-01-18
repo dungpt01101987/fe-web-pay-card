6a02393bafeae2866d9d22ca9b5cb3e0e6f47cc8
PhamTienDung <phamdung110@gmail.com>
2024-01-18 17:02:47 +0700

    update code home

--- src/layouts/components/Header/MidPanel/MidPanel.js
+++ src/layouts/components/Header/MidPanel/MidPanel.js
@@ -21,18 +21,17 @@ const MidPanel = () => {
             </div>
             <div className={cx('search-bar')}>
                 <div className={cx('search-bar-wrap')}>
-                    <form className={cx('search-bar-cmp')}>
-                        <input
-                            type="search"
-                            placeholder="Please enter the name of the product or game"
-                            aria-label="Search"
-                        />
-                        <button className={cx('search-bar-btn')} type="submit">
-                            <i className={cx('el-icon')}>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="currentColor" d="m795.904 750.72 124.992 124.928a32 32 0 0 1-45.248 45.248L750.656 795.904a416 416 0 1 1 45.248-45.248zM480 832a352 352 0 1 0 0-704 352 352 0 0 0 0 704z"></path></svg>
-                            </i>
-                        </button>
-                    </form>
+                    <input
+                        type="search"
+                        placeholder="Please enter the name of the product or game"
+                        aria-label="Search"
+                        className={cx('search-bar-cmp')}
+                    />
+                    <button className={cx('search-bar-btn')} type="submit">
+                        <i className={cx('el-icon')}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="currentColor" d="m795.904 750.72 124.992 124.928a32 32 0 0 1-45.248 45.248L750.656 795.904a416 416 0 1 1 45.248-45.248zM480 832a352 352 0 1 0 0-704 352 352 0 0 0 0 704z"></path></svg>
+                        </i>
+                    </button>
                 </div>
                 <div className={cx('search-keywords')}>
                     <Nav.Link href="#link" className={cx('search-keywords-item')}>Momo</Nav.Link>
